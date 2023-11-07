da084563751e208557d2e23b6ee0b597080fbd17
eli <suicidecoders@gmail.com>
2023-11-07 10:59:23 +0300

    added while loop to continue playing game

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,37 +1,45 @@
-
-#get random value
+#import random module to use random function
 import random
+sky_is_blue = True
 
-option = ('rock', 'paper', 'scissors')
-cpu = random.choice(option)
+#added while loop to keep on repeating game until user enters n to stop playing
+while sky_is_blue:
 
-print('//////////////////-------------------------xSUI---------------')
-user = input('\n\nPlease choose rock,paper or scissor: ')
+    option = ('rock', 'paper', 'scissors')  #list of possible choices
+    cpu = random.choice(option)     #function to generate random values from the list above
 
-#reprompt incase does not type what is among choices
-while user not in option:
-    user = input('Invalid Entry! Please pick among choices: ')
+    print('//////////////////-------------------------xSUI---------------')
+    user = input('\n\nPlease choose rock,paper or scissor: ')
 
-#if statements 
-if user == 'rock' and cpu == 'scissor':
-    print(f'cpu: {cpu}')
-    print(f'user: {user}')
-    print('you won!')
-elif user == 'paper' and cpu == 'rock':
-    print(f'cpu: {cpu}')
-    print(f'user: {user}')
-    print('you won!')
-elif user == 'scissors' and cpu == 'paper':
-    print(f'cpu: {cpu}')
-    print(f'user: {user}')
-    print('you won')
-elif user == cpu:
-    print(f'cpu: {cpu}')
-    print(f'user: {user}')
-    print('you tied')
-else:
-   print(f'cpu: {cpu}')
-   print(f'user: {user}')
-   print('you lost')
+    #reprompt incase does not type what is among choices
+    while user not in option:
+        user = input('Invalid Entry! Please pick among choices: ')
+
+    #if statements 
+    if user == 'rock' and cpu == 'scissor':
+        print(f'cpu: {cpu}')
+        print(f'user: {user}')
+        print('you won!')
+    elif user == 'paper' and cpu == 'rock':
+        print(f'cpu: {cpu}')
+        print(f'user: {user}')
+        print('you won!')
+    elif user == 'scissors' and cpu == 'paper':
+        print(f'cpu: {cpu}')
+        print(f'user: {user}')
+        print('you won')
+    elif user == cpu:
+        print(f'cpu: {cpu}')
+        print(f'user: {user}')
+        print('you tied')
+    else:
+        print(f'cpu: {cpu}')
+        print(f'user: {user}')
+        print('you lost')
+
+#this is to prompt user if he want to continue playing
+    play_again = input('Continue playing? (y/n): ').lower()
+    if not play_again == 'y':   #same as if play_again != 'y' in c language
+        sky_is_blue = False
 
 print('thank you for playing')
